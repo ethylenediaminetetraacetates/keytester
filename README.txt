1767f4ef97f7aa143558ee5b6576342d3fd13af4
ethylenediaminetetraacetates <123036101+ethylenediaminetetraacetates@users.noreply.github.com>
2023-09-15 07:49:44 -0400

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,17 @@
+keytester
+
+To the extent possible under law, the person who associated CC0 with
+keytester has waived all copyright and related or neighboring rights
+to keytester.
+
+You should have received a copy of the CC0 legalcode along with this
+work.  If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+
+
+
+Additional thanks to:
+
+https://github.com/BdR76/GimpSpriteAtlas 
+
+it's a good tool.
---
+++ keytesting.js
@@ -0,0 +1,169 @@
+'use strict';
+
+/*
+keytester
+
+To the extent possible under law, the person who associated CC0 with
+keytester has waived all copyright and related or neighboring rights
+to keytester.
+
+You should have received a copy of the CC0 legalcode along with this
+work.  If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+*/
+
+let game = {}
+
+game.scalefactor = 1;
+
+game.framesrun = 0;
+game.fpsarray = [];
+
+game.objects = []
+
+function beep(hz,ms,vol,type){
+    let ctx = new AudioContext();
+    let osc = ctx.createOscillator();
+    let gain = ctx.createGain();
+
+    osc.connect(gain);
+    gain.connect(ctx.destination);
+
+    osc.frequency.value = hz;
+    osc.type.value = type;
+    gain.gain.value = vol;
+
+    osc.start(ctx.currentTime);
+    osc.stop(ctx.currentTime + (ms/1000));
+}
+
+window.onload = function(){
+
+    console.log("window loaded.");
+    document.querySelector("#canvascontainer").innerHTML = "<canvas id = 'htmlcanvas' class = 'maincanvas'>";
+
+    game.c = document.getElementById("htmlcanvas");
+    game.ctx = game.c.getContext("2d");
+
+    game.c.width = window.innerWidth;
+    game.c.height = window.innerHeight;
+        
+    game.sampimg = new Image();
+    game.sampimg.src = "assets/etc/img.png";
+    //keyss = key spritesheet
+    game.keyss = new Image();
+    game.keyss.src = "assets/keysNEW/keysincomplete.png";
+    game.cursorimg = new Image();
+    game.cursorimg.src = "assets/cursors/cursorsmall.png";
+
+    game.loadfiles([game.sampimg, game.keyss,game.cursorimg],0,true)
+
+}
+
+//sets onload for recursion
+game.loadfiles = function(arr,i,startupthings){
+    if(i==arr.length){
+        console.log("files loaded!")
+
+        console.log("running startup functions...")
+        if(startupthings){
+            inputsetup();
+            requestAnimationFrame(newframe);
+        }
+        console.log("done with startup functions!")
+        
+        return true;
+    }
+    console.log("["+i+"/"+arr.length+"]"+" Loading file: "+arr[i])
+    if(arr){
+        arr[i].onload = function(){
+            game.loadfiles(arr,i+1,startupthings);
+        }
+    }
+}
+
+function newframe(ms){
+    game.ms = ms;
+    game.fpsarray.push(ms)
+    if(game.framesrun == 0){
+        console.log("Started in "+ms+" milliseconds.");
+    }else if(game.framesrun < 5){
+        console.log("Frame "+(game.framesrun+1)+": "+ms+" milliseconds.");
+    }
+
+    /*
+    delayedmouse.x = delayedmouse.x + ((mouse.x)-(delayedmouse.x))/(ms/480);
+    delayedmouse.y = delayedmouse.y + ((mouse.y)-(delayedmouse.y))/(ms/480);
+    if(Math.abs(delayedmouse.x-mouse.x) < 5 || (delayedmouse.y-mouse.y) < 5 && !delayedmouse.snap)
+    {delayedmouse.x = mouse.x;delayedmouse.y = mouse.y;delayedmouse.snap = true;}
+    else if(Math.abs(delayedmouse.x-mouse.x) < 20 || (delayedmouse.y-mouse.y) < 20)
+    {delayedmouse.snap = false;}
+    */
+
+    game.framesrun++;
+
+    game.ctx.fillStyle= "#242424";
+    game.ctx.fillRect(0, 0, game.c.width, game.c.height);
+
+    if(window.innerWidth != game.c.width || window.innerHeight != game.c.height){
+        game.c.width = window.innerWidth;
+        game.c.height = window.innerHeight;
+        game.ctx = game.c.getContext("2d");
+    }
+    if(mouse.down){
+        game.c.requestFullscreen();
+    }
+    
+    
+
+    //drawImage(img, <s>, <d>)
+    //s:source, d: destination
+    //<s/d>x <s/d>y <s/d>Width <s/d>Height
+    game.ctx.drawImage(game.keyss,0,((3000-ms*1)%6000)+3000);
+
+    
+    for(let i = 0; i > game.objects.length; i++){
+        switch(game.objects[i].kind){
+
+            case 0:
+                game.ctx.drawImage(game.keyss, 400,0, 400,400, game.objects[i].x,game.objects[i].y, 400*game.scalefactor, 400*game.scalefactor);
+                if(!game.objects[i].active){
+                    //fill in with not active code
+                }
+                break;
+
+            default:
+                game.ctx.drawImage(game.sampimg,game.objects[i].x,game.objects[i].y);
+                break;
+        }
+    }
+
+
+    game.ctx.drawImage(game.cursorimg,mouse.x,mouse.y);
+    requestAnimationFrame(newframe);
+}
+
+
+
+
+let keys = {};
+let mouse = {
+    "down":false,
+    x: 0,
+    y: 0
+}
+let delayedmouse = {
+    "down":false,
+    x: 0,
+    y: 0,
+    "snap":false
+}
+
+function inputsetup(){
+
+    window.addEventListener('keydown', (e) => {keys[e.code] = true;} );
+    window.addEventListener('keyup', (e) => {keys[e.code] = false;} );
+
+    game.c.addEventListener('mousedown', () => {mouse.down = true;} );
+    game.c.addEventListener('mouseup', () => {mouse.down = false;} );
+    game.c.addEventListener('mousemove', (e) => {mouse.x = e.clientX; mouse.y = e.clientY;} );
+}
