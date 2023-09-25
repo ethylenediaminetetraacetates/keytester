fa06a71a9cfe366bed0f9f17db5f30934ca70f58
ethylenediaminetetraacetates <123036101+ethylenediaminetetraacetates@users.noreply.github.com>
2023-09-25 11:28:37 -0400

    small update
    changes it so that it keeps it pressed down for some reason?

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function clicked(){
+    document.querySelector("body > div > button").dispatchEvent(new Event("mousedown"));
     setTimeout(function(){
+        document.querySelector("body > div > button").dispatchEvent(new Event("mouseup"));
         document.location = "./keytesting.html";
     },500)
 }
